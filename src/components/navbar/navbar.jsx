92c3e6898000fd6331dfb65a0d83fdb1d080bac3
mac <wubezeleke@gmail.com>
2023-06-16 15:32:09 +0300

    added loginhandler

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Link, NavLink, Routes, Route, useNavigate } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './navbar.css';
 import localforage from 'localforage';
 
 
 const Navbar = () => {
-  const navigate = useNavigate();
 
   const handleLogout = async () => {
     await localforage.removeItem('token');
-    navigate('/login');
   };
-  
+
   return (
     <nav>
       <ul>
         <li> <NavLink to="/dashboard" activeclassname="active">Create Device</NavLink> <br/><br></br> </li>
         <li> <NavLink to="/user" activeclassname="active">User Analysis</NavLink> <br/><br></br> </li>
-        <li><a onClick={handleLogout}>Logout </a> </li>
+        <li><a href="/login" onClick={handleLogout}>Logout </a> </li>
       </ul>
     </nav>
   );
