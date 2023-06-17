2acf6da8f9e994baf6aa45ca714a778559f7e1a8
mac <wubezeleke@gmail.com>
2023-06-17 22:48:06 +0300

    added feedback feature

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,7 +1,95 @@
 import react from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Navbar from '../navbar/navbar';
+import { Container, Row, Col, Table, Button } from 'react-bootstrap';
+import './feedback.css';
+
 
 const Feedback = () => {
+    const [feedbackData, setFeedbackData] = useState(null);
+    const [showfeedback, setShowFeedback] = useState(0);
+    const [loading, setLoading] = useState(false);
+    
+    useEffect(() => {
+        // Fetch initial dashboard data
+        fetchfeedbackData();
+    
+        // Fetch feedback data every minutes
+        const interval = setInterval(fetchfeedbackData, 1 * 60 * 1000);
+        // Clean up the interval on component unmount
+        return () => clearInterval(interval);
+      }, []);
+
+
+    const fetchfeedbackData = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+              window.location.href = '/';
+              return;
+            }
+            const headers = {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+              responseType: 'json',
+            };
+            const response = await axios.get('https://food-dispenser-api.onrender.com/v1/feedback', { headers });
+    
+            if (response.data.message === 'Unauthorized') {
+              localStorage.removeItem('token');
+              window.location.href = '/';
+              return;
+            }
+            
+            const feedbacks = response.data.response;
+            console.log(feedbacks);
+            
+            if (feedbacks !== feedbackData){
+                setFeedbackData(feedbacks);
+            }
+    
+        } catch (error) {
+          console.error('Error fetching Feedback data:', error);
+        }
+      };
+    
+
+    const handleUpdate = async (id, status) => {
+        try {
+            setLoading(true);
+            const token = localStorage.getItem('token');
+            if (!token) {
+                window.location.href = '/';
+                return;
+            }
+            const headers = {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+                responseType: 'json',
+            };
+
+            const body = {
+                status: status,
+            };
+
+            const response = await axios.put(`https://food-dispenser-api.onrender.com/v1/feedback/${id}`, body, { headers });
+            if (response.data.message === 'Unauthorized') {
+                localStorage.removeItem('token');
+                window.location.href = '/';
+                return;
+            }
+            fetchfeedbackData();
+            setLoading(false);
+        } catch (error) {
+            console.error('Error updating feedback:', error);
+            setLoading(false);
+        }
+    };
+
+    const pendingfeedback = !feedbackData ? {} : feedbackData.filter(feedback => feedback.status === 'pending');
+    const approvedfeedback = !feedbackData ? {} : feedbackData.filter(feedback => feedback.status === 'approved');
+
     return (
         <div>
             <Navbar />
@@ -9,6 +97,87 @@ const Feedback = () => {
             <br />
             <br />
             <h1>Feedback</h1>
+            
+            <div className="navButton">
+                <Button variant="outline-primary" className= {showfeedback === 0 ? 'active' : ''} onClick={() => setShowFeedback(0)}>All</Button>
+                <Button variant="outline-primary" className={showfeedback === 1 ? 'active' : ''} onClick={() => setShowFeedback(1)}>Pending</Button>
+                <Button variant="outline-primary" className={showfeedback === 2 ? 'active' : ''} onClick={() => setShowFeedback(2)}>Approved</Button>
+            </div>
+            
+            <Container fluid className='mt-5'>
+            <Row className="mt-5">
+                <Col className="tablecenter" md={{ span: 8, offset: 2 }}>
+                    <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>Created Date and Time</th>
+                        <th>User</th>
+                        <th>Device</th>
+                        <th>Feedback</th>
+                        <th>Rating</th>
+                        <th> Status </th>
+                        {showfeedback === 1 && <th>Update</th>}
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        { (showfeedback === 0) ? (
+                            feedbackData &&
+                                feedbackData.map((feedback) => (
+                                    <tr key={feedback._id}>
+                                    <td>
+                                    {new Date(feedback.date).toLocaleDateString()}{" "}
+                                    {new Date(feedback.date).toLocaleTimeString()}
+                                    </td>
+                                    <td>{feedback.user.full_name}</td>
+                                    <td>{feedback.device._id}</td>
+                                    <td>{feedback.feedback}</td>
+                                    <td>{feedback.rating}</td>
+                                    <td> {feedback.status} </td>
+                                    </tr>
+                                )) ) :
+
+                            (showfeedback === 1) ? (
+                                    pendingfeedback &&
+                                    pendingfeedback.map((feedback) => (
+                                        <tr key={feedback._id}>
+                                        <td>
+                                        {new Date(feedback.date).toLocaleDateString()}{" "}
+                                        {new Date(feedback.date).toLocaleTimeString()}
+                                        </td>
+                                        <td>{feedback.user.full_name}</td>
+                                        <td>{feedback.device._id}</td>
+                                        <td>{feedback.feedback}</td>
+                                        <td>{feedback.rating}</td>
+                                        <td> {feedback.status} </td>
+                                        <td>
+                                            {loading ? <Button variant="outline-primary" disabled> Approving... </Button> : <Button variant="outline-primary" onClick={() => {handleUpdate(feedback._id, 'approved')}}> Approve </Button> } </td> 
+                                        </tr>
+                                    )))  : 
+                            
+                                    (approvedfeedback &&
+                                        approvedfeedback.map((feedback) => (
+                                            <tr key={feedback._id}>
+                                            <td>
+                                            {new Date(feedback.date).toLocaleDateString()}{" "}
+                                            {new Date(feedback.date).toLocaleTimeString()}
+                                            </td>
+                                            <td>{feedback.user.full_name}</td>
+                                            <td>{feedback.device._id}</td>
+                                            <td>{feedback.feedback}</td>
+                                            <td>{feedback.rating}</td>
+                                            <td> {feedback.status} </td>
+                                            </tr>
+                                        ))   
+                                    )
+                        
+                        }
+                        
+                    </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            </Container>
         </div>
     )
 }
